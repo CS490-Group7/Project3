e1c17b451e8081a0b92cdae091286b0e7702f873
EC2 Default User <ec2-user@ip-172-31-83-156.ec2.internal>
2021-04-08 16:48:38 +0000

    added logout button

---
+++ src/Logout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {GoogleLogout} from 'react-google-login';
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
+function Logout()
+{
+    const onSuccess=()=>
+    {
+        alert('Logout made successfully');
+    };
+    
+    return(
+        <div>
+        <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSucess={onSuccess}
+        style ={{marginTop: '100px'}}
+        ></GoogleLogout>
+        </div>
+        
+        );
+}
+export default Logout;
