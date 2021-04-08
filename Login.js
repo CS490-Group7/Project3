443b25f6fc687d9a4c0ab66992d6cc575dda4a88
EC2 Default User <ec2-user@ip-172-31-83-156.ec2.internal>
2021-04-08 16:45:01 +0000

    added login button

---
+++ Login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import dotenv from 'dotenv';
+import {GoogleLogin} from 'react-google-login';
+
+import {refreshTokenSetup} from './refreshToken.js';
+
+const clientId = process.env.REACT_APP_CLIENT_ID;
+console.log(clientId)
+
+function Login()
+{
+    const onSuccess= (res)=>
+    {
+        console.log('[Login Success] currentUser:',res.profileObj);
+        // refreshed token after an hour
+        refreshTokenSetup(res);
+    };
+    const onFailure = (res)=> 
+    {
+        console.log('[Login failed] res:',res);
+        
+    }
+    return (
+        <div>
+        <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy ={'single_host_origin'}
+        style ={{marginTop: '100px'}}
+        isSignedIn={true}
+        
+        />
+        </div>
+        
+        
+        );
+}
+export default Login;
